d77ac556b423343f63f6f7e7db4f8c9b928bbad8
“Willie” <willie.williams@codeimmersives.com>
2021-03-11 06:58:13 -0500

    finished

--- main.js
+++ main.js
@@ -2,6 +2,44 @@
 // │ Your code here! │	
 // └─────────────────┘
 
+function isAdmin (user){
+    return user.userRole === 'ADMIN'
+}
+
+function getEmail(user){
+    return (user.firstName + '.' +user.lastName + "@codeimmersives.com").toLowerCase()
+}
+
+function getPlaylistLength(playList){
+    return playList.songs.length
+
+}
+
+function getHardestHomework(arrObj){
+    if (arrObj.length === 0) {return ""}
+
+    let hardestHW = arrObj[0].name
+    let minScore = arrObj[0].averageScore
+
+
+    for (const eachObj of arrObj){
+        if (eachObj.averageScore < minScore){  
+            minScore =  eachObj.averageScore 
+            hardestHW =  eachObj.name   
+        }
+    }
+    return hardestHW
+}
+
+function createPhonebook(name, number){
+    const phoneBook = {
+    }
+        for (let i = 0;i < name.length; i++){
+        phoneBook[name[i]] = number[i]
+        }
+    console.log(phoneBook)
+    return phoneBook
+}
 
 
 // ┌─────────────────────────────────────┐
